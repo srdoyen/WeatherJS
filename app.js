1cbf4e2a80875a6a1d2841eef239b0c81d950bf7
srdoyen <srdoyen@gmail.com>
2019-10-27 17:25:59 -0700

    weather app complete

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-//Init weather object
-const WeatherJS = new Weather("Seattle", "us");
+//Init localstorage
+const storage = new Storage();
+//Get stored location data
+const weatherLocation = storage.getLocationData();
+const WeatherJS = new Weather(weatherLocation.city, weatherLocation.country);
+
 const ui = new UI();
 //Get weather when DOM loads
 document.addEventListener("DOMContentLoaded", getWeather);
@@ -7,7 +11,11 @@ document.addEventListener("DOMContentLoaded", getWeather);
 document.getElementById("w-change-btn").addEventListener("click", e => {
   const city = document.getElementById("city").value;
   const country = document.getElementById("country").value;
+  //Change Location
   WeatherJS.changeLocation(city, country);
+
+  //Set location in LS
+  storage.setLocationData(city, country);
   //Get and display weather
   getWeather();
   //Close Modal
