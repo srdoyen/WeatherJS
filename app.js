cfb128c4784988de3a829ea2c752f811dfe4b195
srdoyen <srdoyen@gmail.com>
2019-10-27 17:10:15 -0700

    ui updates

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const WeatherJS = new Weather("Seattle", "us");
 const ui = new UI();
 //Get weather when DOM loads
 document.addEventListener("DOMContentLoaded", getWeather);
+//Change location
+document.getElementById("w-change-btn").addEventListener("click", e => {
+  const city = document.getElementById("city").value;
+  const country = document.getElementById("country").value;
+  WeatherJS.changeLocation(city, country);
+  //Get and display weather
+  getWeather();
+  //Close Modal
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   WeatherJS.getWeather()
